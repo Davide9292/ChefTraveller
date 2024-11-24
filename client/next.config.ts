b8fb95f5718b1298ca8debde5a538f1d6ef6b439
Davide Pedonw <davidepedone1@gmail.com>
2024-11-24 22:00:22 +0100

    rimesso vercel.json

---
+++ client/next.config.ts
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true, 
+  images: {
+    domains: ['randomuser.me'], // Add any other domains you're using for images
+  },
+};
+
+module.exports = nextConfig;
