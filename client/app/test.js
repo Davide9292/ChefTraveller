44fff02e6e798a8b65ae64378c9ac1bd74adbbdc
Davide Pedonw <davidepedone1@gmail.com>
2024-11-24 23:51:57 +0100

    test.js

---
+++ client/app/test.js
@@ -0,0 +1,8 @@
+export default function TestPage() {
+    return (
+      <div>
+        <h1>This is a test page</h1>
+        <p>If you can see this, the deployment is working correctly.</p>
+      </div>
+    );
+  }
