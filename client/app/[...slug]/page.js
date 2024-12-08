9af10991e272e319b436ad022277626598643b08
Davide Pedonw <davidepedone1@gmail.com>
2024-12-08 12:54:21 +0100

    updated [...slug]

--- client/app/[...slug]/page.js
+++ client/app/[...slug]/page.js
@@ -1,4 +1,9 @@
 // client/app/[...slug]/page.js
-export default function Page() {
-    return <div>This is a dynamic route</div>;
+export default function Page({ params }) {
+    return (
+      <div>
+        <h1>This is a dynamic route</h1>
+        <p>Slug: {params.slug}</p> 
+      </div>
+    );
   }
