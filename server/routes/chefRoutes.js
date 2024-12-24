bd0a170975bd52c210131e818a7aaa29be8219e4
Davide Pedonw <davidepedone1@gmail.com>
2024-12-24 14:54:06 +0100

    fix

--- server/routes/chefRoutes.js
+++ server/routes/chefRoutes.js
@@ -1,9 +1,12 @@
+
 const express = require('express');
 const router = express.Router();
+const authMiddleware = require('../middleware/authMiddleware');
 const chefController = require('../controllers/chefController');
 
 router.get('/', chefController.getChefs);
 router.get('/:id', chefController.getChefById);
+router.get('/me', authMiddleware, chefController.getChefProfile);
 router.put('/:id/availability', authMiddleware, chefController.updateAvailability); // Add this route
 
 
